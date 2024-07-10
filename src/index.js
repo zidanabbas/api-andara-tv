4da484eff2dd569bf7a62c11cac36864311a3e4f
zidanabbas <zidan.abbas28@gmail.com>
2024-07-11 00:52:22 +0700

    changes file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import prisma from "../prisma/prisma.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,8 +9,9 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello Worlds!");
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 app.listen(PORT, () => {
