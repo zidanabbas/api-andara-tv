d202b1bdbabdf521e43ab47029e99beddeddb1ee
zidanabbas <zidan.abbas28@gmail.com>
2024-07-16 00:33:24 +0700

    disable routes update and delete

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,13 +4,14 @@ import {
   getAllUsers,
   getUserById,
   createUser,
-  updateUser,
-  deleteUser,
-} from "../controllers/userController.js";
+  // updateUser,
+  // deleteUser,
+} from "../controllers/userControllers.js";
+
 router.get("/users", getAllUsers);
 router.get("/users/:userId", getUserById);
 router.post("/users", createUser);
-router.patch("/users/:userId", updateUser);
-router.delete("/users/:userId", deleteUser);
+// router.patch("/users/:userId", updateUser);
+// router.delete("/users/:userId", deleteUser);
 
 export default router;
